38458d5b1138ca1b8e09d2959bb94d65f506d749
ddgutierrez <113035923+ddgutierrez@users.noreply.github.com>
2022-11-04 18:00:33 -0500

    Add files via upload

--- ClosestPair.java
+++ ClosestPair.java
@@ -9,6 +9,9 @@ import java.io.File;
 import java.io.FileNotFoundException;
 import java.io.IOException;
 import java.io.PrintWriter;
+import java.util.ArrayList;
+import java.util.Comparator;
+import java.util.List;
 import java.util.Random;
 
 /**
@@ -17,11 +20,11 @@ import java.util.Random;
  */
 public class ClosestPair {
 
-    static long[] tiempos = new long[100];
-    static long[] comps = new long[100];
-    static long[] tiempos2 = new long[100];
-    static long[] comps2 = new long[100];
-    static int sum = 0;
+    static long[] tiempos = new long[10];
+    static long[] comps = new long[10];
+    static long[] tiempos2 = new long[10];
+    static long[] comps2 = new long[10];
+    static long sum = 0;
 
     /**
      * @param args the command line arguments
@@ -29,30 +32,29 @@ public class ClosestPair {
     public static void main(String[] args) {
         create("brute");
         create("divide");
-        //realiza 100 repeticiones para cada caso variando tamano
-        for (int repeticiones = 0; repeticiones < 1; repeticiones++) {
+        //realiza 10 repeticiones para cada caso variando tamano
+        for (int repeticiones = 0; repeticiones < 8; repeticiones++) {
+            int tam =(int) Math.pow(4,repeticiones+1);
             //genera n coordenadas aleatorias en la funcion create
-            int cords[][] = create(6 * (repeticiones + 1));
+            ArrayList <Punto> coordenadas = create(tam);
             //llamado a bruteforce para calcular distancia minima
-            System.out.println(bruteforce(cords, repeticiones, 0, 0, cords.length - 1));
+            bruteforce(coordenadas, repeticiones, 0);
             //guarda tiempo de ejecucion de la repeticion
             tiempos[repeticiones] = sum;
             sum = 0;
-            show(cords);
-            //organiza la matriz para realizar la separacion
-            bubbleSort(cords);
-            show(cords);
+            //organiza las coordenadas
+            coordenadas.sort(Comparator.comparing(Punto->Punto.x));
             //llama subrutina encargada de separar matriz y calcular distancia minima
-            DAC(cords, repeticiones);
+            //DAC(coordenadas, repeticiones,1000000);
             //guarda tiempo de ejecucion
             tiempos2[repeticiones] = sum;
             sum = 0;
+            System.out.println(repeticiones);
         }
-        /*
         try {
             PrintWriter out = new PrintWriter("brute.txt");
             for (int i = 0; i < tiempos.length; ++i) {
-                int s = ((i + 1) * 6);
+                int s = (int) Math.pow(4, i+1);
                 //Escribir cada tiempo de ejecucion en el archivo
                 out.println(s + " " + tiempos[i] + " " + comps[i]);
             }
@@ -64,7 +66,7 @@ public class ClosestPair {
         try {
             PrintWriter out = new PrintWriter("divide.txt");
             for (int i = 0; i < tiempos2.length; ++i) {
-                int s = ((i + 1) * 6);
+                int s = (int) Math.pow(4, i+1);
                 //Escribir cada tiempo de ejecucion en el archivo
                 out.println(s + " " + tiempos2[i] + " " + comps2[i]);
             }
@@ -72,41 +74,42 @@ public class ClosestPair {
         } catch (FileNotFoundException err) {
             // complains if file does not exist
             err.printStackTrace();
-        }*/
+        }
     }
 
-    //funcion que retorna una matriz con 6 coordenadas aleatorias
-    public static int[][] create(int n) {
-        //crea matriz de 6 columnas y 2 filas
-        int coordenadas[][] = new int[n][2];
-        //ciclo para generar 6 coordenadas
+    //funcion que retorna un ArrayList con n coordenadas aleatorias
+    public static ArrayList<Punto> create(int n) {
+        ArrayList <Punto> creados= new ArrayList<Punto>();
+        //ciclo para generar n coordenadas
         for (int i = 0; i < n; i++) {
             Random randomGenerator = new Random();
-            //asigna las coordenadas a las filas del punto
-            coordenadas[i][0] = randomGenerator.nextInt(n);
-            coordenadas[i][1] = randomGenerator.nextInt(n);
+            Punto p = new Punto(0,0);
+            //asigna las coordenadas a los puntos
+            Punto.x = randomGenerator.nextInt(n);
+            Punto.y = randomGenerator.nextInt(n);
+            creados.add(p);
         }
-        return coordenadas;
+        return creados;
     }
 
     //funcion que calcula la distancia minima entre dos puntos i y j
-    public static double distancias(int c[][], int i, int j) {
+    public static double distancias(Punto i,Punto j) {
         //retorna la distancia
-        return Math.sqrt(Math.pow(c[i][0] - c[j][0], 2) + Math.pow(c[i][1] - c[j][1], 2));
+        return Math.sqrt(Math.pow(i.x - j.x, 2) + Math.pow(i.y - j.y, 2));
     }
 
     //algoritmo bruteforce para encontrar distancias minimas entre todos los puntos
-    public static double bruteforce(int c[][], int index, int ca, int inicio, int fin) {
+    public static double bruteforce(List<Punto> coordenadas, int index,int ca) {
         //inicializar una distancia minima
         double d_min = 10000000;
         double d = 0;
         int comp = 0;
-        //ciclos anidados para recorrer matriz y encontrar distancias minimas entre todos los puntos
-        for (int i = inicio; i < fin - 1; i++) {
-            for (int j = i + 1; j < fin; j++) {
-                //llama funcion que calcula distancia minima entre dos puntos
-                d = distancias(c, i, j);
-                //compara si la distancia entre i y j es menor que la menor encontrada
+        //ciclos anidados para recorrer las coordenadas
+        for(int i=0;i<coordenadas.size();i++){
+            for(int j=i+1;j<coordenadas.size();j++){
+                //calcula distancia entre las dos coordenadas
+                d=distancias(coordenadas.get(i),coordenadas.get(j));
+                //aumenta el numero de comparaciones
                 comp++;
                 long startTime = System.nanoTime();
                 if (d < d_min) {
@@ -115,59 +118,41 @@ public class ClosestPair {
                 }
                 long finalTime = System.nanoTime() - startTime;
                 sum += finalTime;
+                j++;
             }
         }
-        if (ca == 0) {
-            comps[index] = comp;
-        } else {
-            comps2[index] = comp;
+        if(ca==0){
+            comps[index]+=comp;
+        }else{
+            comps2[index] += comp;
         }
         //retorna la distancia minima encontrada
         return d_min;
     }
 
-    static void show(int m[][]) {
-        for (int i = 0; i < m.length; i++) {
-            System.out.println(m[i][0] + " " + m[i][1]);
+    public static double DAC(List<Punto> coordenadas, int r,double d) {
+        double dmin=d;
+        //busca el elemento de la mitad
+        int pos=coordenadas.size()/2;
+        if(coordenadas.size()%2==1){
+            pos+=1;
         }
-    }
-
-    static void bubbleSort(int[][] mat) {
-        //longitud de la matriz
-        int n = mat.length;
-        //variables temporales para el cambio entre elementos
-        int temp = 0;
-        int temp2 = 0;
-        //ciclos anidados para recorrer matriz
-        for (int i = 0; i < n; i++) {
-            for (int j = 1; j < (n - i); j++) {
-                //comparacion en coordenada x
-                if (mat[j - 1][0] > mat[j][0]) {
-                    //intercambia elementos  
-                    temp = mat[j - 1][0];
-                    temp2 = mat[j - 1][1];
-                    mat[j - 1][0] = mat[j][0];
-                    mat[j - 1][1] = mat[j][1];
-                    mat[j][0] = temp;
-                    mat[j][1] = temp2;
-                }
-            }
+        //en caso de que hayan mas de 3 coordenadas se subdivide
+        if(coordenadas.size()>3){
+            //se crean listas para ambas mitades
+            List<Punto> p1 = coordenadas.subList(0, pos);
+            List<Punto> p2 = coordenadas.subList(pos, coordenadas.size());
+            //se encuentra la distancia minima entre las dos sublistas
+            dmin=Math.min(DAC(p1,r,d),DAC(p2,r,d));
+            //se encuentra la distancia minima en comparacion a las coordenadas de la mitad
+            dmin=Math.min(dmin,distancias(p1.get(p1.size()-1),p2.get(0)));
         }
-    }
-
-    public static void DAC(int m[][], int r) {
-        //calcula posicion de la mitad
-        int pos=(m.length-1)/2;
-        //realiza bruteforce para la primera mitad de la matriz
-        double d1 = bruteforce(m, r, 1, 0, pos);
-        //realiza bruteforce para la segunda mitad de la matriz
-        double d2 = bruteforce(m, r, 1, pos, m.length - 1);
-        //encuentra distancia minima entre los dos puntos de la mitad
-        double d3 = Math.sqrt(Math.pow(m[pos][0] - m[pos+1][0], 2) + Math.pow(m[pos][1] - m[pos+1][1], 2));
-        //encuentra minimo entre las 3 distancias encontradas
-        double d = Math.min(d1, d2);
-        d = Math.min(d, d3);
-        System.out.println(d);
+        else
+        {
+            //realiza bruteforce y encuentra minimo entre las distancias encontradas
+            dmin = Math.min(dmin,bruteforce(coordenadas, r,1));
+        }
+        return dmin;
     }
 
     private static void create(String r) // creates a file
